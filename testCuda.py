63d9e2af632ad6a617c156779198ef5f4484cfd1
R15i <55852422+r15i@users.noreply.github.com>
2025-07-29 21:59:30 +0200

    documented testCuda.py

--- testCuda.py
+++ testCuda.py
@@ -1,14 +1,19 @@
 
 import torch
+
 print(torch.__version__)
 print(torch.cuda.is_available())
 print(torch.cuda.get_device_name(0)) # Prints the name of your first GPU
 print(torch.version.cuda) # Prints the CUDA version PyTorch was built with
 
 
-#def main():
-#     print("Hello from deeplearnigmidi!")
+# expected output 
 
 
-#if __name__ == "__main__":
-#    main()
+"""
+2.7.1+cu128
+True
+NVIDIA GeForce GTX 850M
+12.8
+"""
+
